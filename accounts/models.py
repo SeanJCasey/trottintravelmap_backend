4c443d4922fbe77e51a7dc76a30a281a5a0ce0d2
Sean Casey <sean@seanjcasey.com>
2019-02-26 11:13:43 -0500

    added name and slug fields to user account and updated its serializer

--- accounts/models.py
+++ accounts/models.py
@@ -4,20 +4,26 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 from django.db import models
 
 from django_extensions.db.models import TimeStampedModel
+from django.utils.text import slugify
 
 
 class CustomUserManager(BaseUserManager):
 
     def _create_user(self,
                      email,
+                     name,
                      password=None,
                      is_superuser=False
                      ):
         if not email:
             raise ValueError('You must provide an email address.')
 
+        if not name:
+            raise ValueError('You must provide a name.')
+
         user = self.model(
             email=self.normalize_email(email),
+            name=name,
             is_superuser=is_superuser
         )
 
@@ -25,16 +31,18 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, password=None):
-        return self._create_user(email, password)
+    def create_user(self, email, name, password=None):
+        return self._create_user(email, name, password)
 
     def create_superuser(self,
                          email,
+                         name,
                          password,
                          is_superuser=True
                          ):
         return self._create_user(
             email,
+            name,
             password,
             is_superuser
         )
@@ -43,6 +51,8 @@ class CustomUserManager(BaseUserManager):
 class CustomUser(AbstractBaseUser, TimeStampedModel, PermissionsMixin):
     email = models.EmailField(unique=True)
     is_active = models.BooleanField(default=True)
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True, max_length=70, null=True)
 
     objects = CustomUserManager()
 
@@ -52,3 +62,19 @@ class CustomUser(AbstractBaseUser, TimeStampedModel, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+    def _generate_unique_slug(self):
+        slug = orig = slugify(self.name)
+        counter = 1
+        while CustomUser.objects.filter(slug=slug).exists():
+            slug = '{}-{}'.format(orig, counter)
+            counter += 1
+        return slug
+
+    def save(self, *args, **kwargs):
+        print(self.slug)
+        print(slugify(self.name))
+        if not self.slug:
+            self.slug = self._generate_unique_slug()
+
+        super().save(*args, **kwargs)
--- api/serializers.py
+++ api/serializers.py
@@ -35,55 +35,27 @@ class PlaceMapSerializer(serializers.ModelSerializer):
                             'un_country_count', 'un_country_area_percent')
 
 
-    # def create(self, validated_data):
-    #     request = self.context.get('request')
-    #     user = request.user
-    #     places = validated_data['places']
-
-    #     placemap = PlaceMap.objects.create(user=user)
-    #     placemap.places.add()
-
-    #     return 
-    #         user=user,
-    #         )
-
-    #     if user != request.user:
-    #         raise serializers.ValidationError({
-    #             'user': 'User can only create placemap for self.'
-    #         })
-
-
-    # def validate(self, data):
-    #     user = data.get('user')
-
-    #     if user:
-    #         request = self.context.get('request')
-
-    #         if user != request.user:
-    #             raise serializers.ValidationError({
-    #                 'user': 'User can only create placemap for self.'
-    #             })
-
-    #     return super().validate(data)
-
-
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'email', 'password', 'placemap')
-        read_only_fields = ('id', 'placemap')
+        fields = ('id', 'email', 'name', 'password', 'placemap', 'slug')
+        read_only_fields = ('id', 'placemap', 'slug')
 
     def validate(self, data):
         password = data.get('password')
-        validate_password(
-            password,
-            password_validators=[MinimumLengthValidator()]
-        )
+        if password:
+            validate_password(
+                password,
+                password_validators=[MinimumLengthValidator()]
+            )
 
         return super().validate(data)
 
     def create(self, validated_data):
         return User.objects.create_user(
-            validated_data['email'], validated_data['password'])
+            validated_data['email'],
+            validated_data['name'],
+            validated_data['password']
+        )
