b3b7449c8c07b7d2b80c382d3e28df899d3109ff
Sean Casey <sean@seanjcasey.com>
2019-04-02 17:17:51 -0400

    added whitenoise and static files config to play nice with heroku

--- settings/base.py
+++ settings/base.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
@@ -125,13 +126,21 @@ USE_L10N = True
 
 USE_TZ = False
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
+# =============================================================================
 # Static files (CSS, JavaScript, Images)
+# =============================================================================
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-AUTH_USER_MODEL = 'accounts.CustomUser'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # =============================================================================
 # Django Rest Framework
